5639477616f9100a9e01fe6484b34021f6a37d49
hogsmill <info@hogsmill.com>
2020-10-24 09:32:11 +0100

    change keep to run under crontab

--- keep.sh
+++ keep.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 apps='apps.txt'
 
-while true
+while read line
 do
-  while read line
-  do
-    if [[ ! $line =~ ^\# ]]
-    then
-      port=`echo $line | cut -d, -f1`
-      dir=`echo $line | cut -d, -f2`
-      app=`echo $line | cut -d, -f3`
-
-      running=`ps -ef | grep node | grep "$app" | grep -v grep`
-      if [ $? -ne 0 ]
-      then
-        echo "CRASHED! Re-starting \"$app\" server at `date`"
-        node /usr/apps/$dir/src/server.js $port $app &
-        ps -ef | grep node  | sort -k10
-      fi
-    fi
-  done < $apps
-
-  mongo=`ps -ef | grep mongod | grep -v grep`
-  if [ $? -ne 0 ]
+  if [[ ! $line =~ ^\# ]]
   then
-    echo "MONGO CRASHED! Re-starting at `date`"
-    /usr/bin/mongod --config /etc/mongod.conf &
-  fi
+    port=`echo $line | cut -d, -f1`
+    dir=`echo $line | cut -d, -f2`
+    app=`echo $line | cut -d, -f3`
+    logFile="/usr/apps/logs/$dir.log"
 
-  sleep 10
-done
+    running=`ps -ef | grep node | grep "$app" | grep -v grep`
+    if [ $? -ne 0 ]
+    then
+      echo "CRASHED! Re-starting \"$app\" server at `date`"
+      node /usr/apps/$dir/src/server.js $port $app $logFile &
+      ps -ef | grep node  | sort -k10
+    fi
+  fi
+done < $apps
+
+mongo=`ps -ef | grep mongod | grep -v grep`
+if [ $? -ne 0 ]
+then
+  echo "MONGO CRASHED! Re-starting at `date`"
+  /usr/bin/mongod --config /etc/mongod.conf &
+fi
