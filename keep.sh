18061b312edb71df446d792fad6534bd9ccb474c
hogsmill <info@hogsmill.com>
2021-10-10 18:10:51 +0100

    apps

--- keep.sh
+++ keep.sh
@@ -36,7 +36,6 @@ do
     app=`echo $line | cut -d, -f4`
     logFile="/usr/apps/logs/$dir.log"
 
-    echo "ps -ef | grep node | grep \"$port $app\" | grep -v grep"
     running=`ps -ef | grep node | grep "$port $app" | grep -v grep`
     if [ $? -ne 0 ]
     then
