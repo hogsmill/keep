24ca1088e6dfa780db4e90a64ebfec6670685695
hogsmill <info@hogsmill.com>
2021-10-10 15:17:26 +0100

    customer apps

--- keep.sh
+++ keep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apps='/usr/keep/apps.txt'
+customerApps='/usr/keep/customerApps.txt'
 
 while read line
 do
@@ -19,6 +20,25 @@ do
     fi
   fi
 done < $apps
+
+while read line
+do
+  if [[ ! $line =~ ^\# ]]
+  then
+    server=`echo $line | cut -d, -f1`
+    port=`echo $line | cut -d, -f2`
+    dir=`echo $line | cut -d, -f3`
+    app=`echo $line | cut -d, -f4`
+    logFile="/usr/apps/logs/$dir.log"
+
+    running=`ps -ef | grep node | grep "$port $app" | grep -v grep`
+    if [ $? -ne 0 ]
+    then
+      echo "Re-started \"$app\" server at `date`"
+      node /usr/apps/$dir/src/server.js $port "$app" $logFile &
+    fi
+  fi
+done < $customerApps
 
 mongo=`ps -ef | grep mongod | grep -v grep`
 if [ $? -ne 0 ]
