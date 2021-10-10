33a4ffd792044cc45895d2245fe3c2f6831cffc4
hogsmill <info@hogsmill.com>
2021-10-10 14:58:58 +0100

    new keep

---
+++ keep.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+
+function getCustomers() {
+  return JSON.parse(fs.readFileSync('customers.json', 'utf8'))
+}
+
+function getApps() {
+  return JSON.parse(fs.readFileSync('apps.json', 'utf8'))
+}
+
+function customerRoutes(customer, apps) {
+  const customerApps = []
+  for (let i = 0; i < apps.length; i++) {
+    const app = apps[i]
+    const route = app.route + '-' + customer.route
+    app.route = route
+    customerApps.push(app)
+  }
+  return customerApps
+}
+
+function getCustomerApps(customer, apps) {
+  let customerApps = []
+  const useApps = apps.find((a) => {
+    return a.name == 'Use'
+  }).apps
+  customerApps = customerApps.concat(useApps)
+  if (customer.level == "Regular Use") {
+    const regularUseApps = apps.find((a) => {
+      return a.name == 'Regular Use'
+    }).apps
+    customerApps = customerApps.concat(regularUseApps)
+  }
+  return customerApps
+}
+
+function customerPorts(apps) {
+  let customerApps = []
+  for (let i = 0; i < apps.length; i++) {}
+    const app = apps[i]
+    envFile = fs.readFileSync('/usr/apps/' + app.route + '.env', 'utf8')
+    console.log(envFile)
+  }
+}
+
+const customers = getCustomers()
+for (let i = 0; i < customers.length; i++) {
+  const customer = customers[i]
+  console.log(customer.name)
+  let customerApps = getCustomerApps(customer, getApps())
+  customerApps = customerRoutes(customer, customerApps)
+  customerApps = customerPorts(customerApps)
+}
