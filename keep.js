e5a98038685953749672d615268022f7dc83aae1
hogsmill <info@hogsmill.com>
2021-10-14 09:37:08 +0100

    all level

--- keep.js
+++ keep.js
@@ -69,7 +69,7 @@ for (let i = 0; i < customers.length; i++) {
   for (j = 0; j < customerApps.length; j++) {
     const route = customer.route ? customer.route : 'default'
     const app = customerApps[j]
-    const appDef = [customer.route, app.port, app.route, app.name].join(',') + '\n'
+    const appDef = [route, app.port, app.route, app.name].join(',') + '\n'
     fs.writeFile('customerApps.txt', appDef, { flag: 'a+' }, err => {})
   }
 }
